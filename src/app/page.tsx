692592644136ca8d6006e753f7d2c5424eb5093a
Poonyawatt Klumnaim <poon@pnwttklm.com>
2024-11-17 20:08:09 +0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import { BsArrowUpRight, BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 interface TEDTalk {
   title: string;
@@ -59,72 +60,48 @@ export default function SearchPage() {
 
   return (
     <div className="p-4">
-      <Image src='/logo.svg' alt="TEDx" width={300} height={300} />
-      <div className="mt-4">
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search in any language..."
-          className="border p-2 rounded w-full"
-        />
-        <h1>Page: {page}</h1>
-        <select
-          value={sourceLang}
-          onChange={(e) => setSourceLang(e.target.value)}
-          className="mt-2 p-2 border rounded"
-        >
-          <option value="th">Thai</option>
-          <option value="en">English</option>
-          <option value="es">Spanish</option>
-          <option value="fr">French</option>
-          <option value="zh">Chinese</option>
-          {/* Add more languages as needed */}
-        </select>
-
-        <button
-          onClick={() => {
-            if (page > 1) {
-              setPage(page - 1);
+      <Image src="/logo.svg" alt="TEDx" width={500} height={500} />
+      <div className="mt-4 flex flex-col justify-center">
+        <div className="space-x-4 flex flex-row justify-center">
+          <select
+            value={sourceLang}
+            onChange={(e) => setSourceLang(e.target.value)}
+            className="p-2 border-4 py-12 rounded-none border-black"
+          >
+            <option value="th">🇹🇭 Thai</option>
+            <option value="en">🇺🇸 English</option>
+            <option value="es">🇪🇸 Spanish</option>
+            <option value="fr">🇫🇷 French</option>
+            <option value="zh">🇨🇳 Chinese</option>
+          </select>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search TED Talks here..."
+            className="border-4 p-2 w-4/12 rounded-none border-black"
+          />
+          <button
+            onClick={() => {
+              setPage(1); // Reset to page 1 on new search
               handleSearch();
-            }
-          }}
-          className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
-          disabled={page === 1}
-        >
-          Previous
-        </button>
-        <button
-          onClick={() => {
-            setPage(page + 1);
-            handleSearch();
-          }}
-          className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Next
-        </button>
-
-        <select
-          value={show}
-          onChange={(e) => setShow(Number(e.target.value))}
-          className="mt-2 p-2 border rounded"
-        >
-          <option value={6}>6</option>
-          <option value={12}>12</option>
-          <option value={24}>24</option>
-          <option value={48}>48</option>
-          <option value={96}>96</option>
-        </select>
-
-        <button
-          onClick={() => {
-            setPage(1); // Reset to page 1 on new search
-            handleSearch();
-          }}
-          className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Search
-        </button>
+            }}
+            className=" bg-red-600 px-12 rounded-none text-white py-3"
+          >
+            Search
+          </button>
+          <select
+            value={show}
+            onChange={(e) => setShow(Number(e.target.value))}
+            className="p-2 border-4 py-12 rounded-none border-black"
+          >
+            <option value={6}>6</option>
+            <option value={12}>12</option>
+            <option value={24}>24</option>
+            <option value={48}>48</option>
+            <option value={96}>96</option>
+          </select>
+        </div>
       </div>
 
       {translatedQuery && (
@@ -133,19 +110,19 @@ export default function SearchPage() {
         </p>
       )}
 
-      <div className="mt-6 w-full grid grid-cols-3 grid-flow-row sm:overflow-auto">
+      <div className="mt-6 w-full grid grid-cols-3 grid-flow-row sm:overflow-auto justify-center">
         {results.map((result, index) => (
-          <div key={index} className="mb-4 w-6/12">
+          <div key={index} className="mb-4 w-6/12 space-y-3">
             <iframe
               src={result._source.url.replace("https://www.", "https://embed.")}
               className="w-full"
             ></iframe>
-            <div>
+            <div className="space-y-3">
               <div className="flex flex-row justify-between">
                 <div>
-                  <h2 className="font-semibold text-lg">
+                  <h1 className="font-bold text-xl">
                     {result._source.title}
-                  </h2>
+                  </h1>
                 </div>
                 <div>
                   <p className="text-white bg-red-600 w-fit px-1">
@@ -155,7 +132,7 @@ export default function SearchPage() {
               </div>
               <div className="flex flex-row justify-between">
                 <div>
-                  <p>{result._source.speaker_1}</p>
+                  <p className="text-gray-400">{result._source.speaker_1}</p>
                 </div>
                 <div>
                   <p>{formatDate(result._source.recorded_date)}</p>
@@ -169,11 +146,39 @@ export default function SearchPage() {
               rel="noopener noreferrer"
               className="text-blue-600 underline"
             >
-              Watch Here
+              <BsArrowUpRight className="text-black" size={32} />
             </a>
           </div>
         ))}
       </div>
+      {translatedQuery && (
+        <div className="flex flex-row justify-center space-x-12">
+          <button
+            onClick={() => {
+              if (page > 1) {
+                setPage(page - 1);
+                handleSearch();
+              }
+            }}
+            className=" text-black flex flex-row place-items-center py-1 "
+            disabled={page === 1}
+          >
+            <BsChevronLeft className="text-black" size={32} />
+            <h1>Previous</h1>
+          </button>
+          <h1 className="font-bold text-3xl">{page}</h1>
+          <button
+            onClick={() => {
+              setPage(page + 1);
+              handleSearch();
+            }}
+            className=" text-black flex flex-row place-items-center py-1 "
+          >
+            <h1>Next</h1>
+            <BsChevronRight className="text-black" size={32} />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
